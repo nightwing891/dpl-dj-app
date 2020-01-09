d27e19ec7be59df7d477a908cd479eeab69846d5
Chris Anderson <chris.anderson955@gmail.com>
2020-01-08 20:13:20 -0700

    finished auth with logout button in navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
+import Navbar from './components/shared/Navbar';
 import Home from './components/Home';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import FetchUser from './components/auth/FetchUser';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 
 const App = () => (
   <div>
-  <Switch>
-    <Route exact path="/" component={Home}/>
-  </Switch>
+  <Navbar />
+  <FetchUser>
+	  <Switch>
+	    <ProtectedRoute exact path="/" component={Home}/>
+	    <Route exact path="/login" component={Login}/>
+	    <Route exact path="/register" component={Register}/>
+	  </Switch>
+  </FetchUser>
   </div>
 )
 
