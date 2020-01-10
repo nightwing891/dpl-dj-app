42cf8025cd06516a57976bd1572377c4900d31e2
Chris Anderson <chris.anderson955@gmail.com>
2020-01-09 20:24:54 -0700

    created about page, contact form, finished navbar

--- client/src/App.js
+++ client/src/App.js
@@ -8,16 +8,23 @@ import Register from './components/auth/Register';
 import FetchUser from './components/auth/FetchUser';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import RoomIndex from './components/room/RoomIndex';
+import NoMatch from './components/shared/NoMatch';
+import About from './components/shared/About';
+import Contact from './components/shared/Contact';
+
 
 const App = () => (
   <div>
   <Navbar />
   <FetchUser>
 	  <Switch>
-	    <ProtectedRoute exact path="/" component={Home}/>
-	    <Route exact path="/login" component={Login}/>
-	    <Route exact path="/register" component={Register}/>
-      <Route exact path="/rooms" component={RoomIndex}/>
+	    <ProtectedRoute exact path="/" component={Home} />
+	    <Route exact path="/about" component={About} />
+	    <Route exact path="/contact" component={Contact} />
+	    <Route exact path="/login" component={Login} />
+	    <Route exact path="/register" component={Register} />
+      <Route exact path="/rooms" component={RoomIndex} />
+      <Route component={NoMatch} />
 	  </Switch>
   </FetchUser>
   </div>
---
+++ client/src/components/shared/About.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import chris from './images/chris.jpg';
+import somchay from './images/somchay.jpg';
+import gabby from './images/gabby.jpg';
+
+const About = () => (
+	<>
+		<h1>About DPL-DJ-APP</h1>
+		<br />
+		<p>
+		This app is a React on Rails stack web application that connects to the Spotify API 
+		to allow a room of people to add songs to a playlist to listen to together.
+		</p>
+		<br />
+		<h1>Developers behind this app</h1>
+		<br />
+		<h3>Somchay Keomanivong</h3>
+		<img className='img1' src={somchay} alt="Somchay" />
+		<br />
+		<p>About Somchay</p>
+		<br />
+		<h3>Gabriella Algecera</h3>
+		<img className='img2' src={gabby} alt="Gabby" />
+		<br />
+		<p>About Gabriella</p>
+		<br />
+		<h3>Chris Anderson</h3>
+		<img className='img3' src={chris} alt="Chris" />
+		<br />
+		<p>About Chris</p>
+		<br />
+	</>
+)
+
+export default About;
