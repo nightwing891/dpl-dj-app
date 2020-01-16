013375b17426facf7d513c06a5a42e3b1a9b943a
Gabriella Algecera <gabriellaalgecera@Brians-MacBook-Pro.local>
2020-01-15 20:44:46 -0700

    adding songs from search attempt 1

--- client/src/components/playlist/SongSuggestions.js
+++ client/src/components/playlist/SongSuggestions.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-//need to change all this...all of it
+import { Button, Form } from 'semantic-ui-react';
+import axios from 'axios';
+
 const SongSuggestions = ({results, query, toggle}) => {
-    if(results && results.length > 0) {
+    if(results && results.length > 0) {  
         const options = results.map( r => {
             const regex = new RegExp(query, 'gi')
-            const { id, name, description } = r
+            const { title, artist } = r
+            const songs = axios.get(`https://api.spotify.com/v1/tracks/${r.id}`)
+                .then( response => {
+                    return response.data
+                })
+                .catch( err => {
+                    console.log(err)
+                })
+            const add = axios.post(`https://api.spotify.com/v1/tracks/${r.id}`)
+                    .then( response => {
+                        return response.data
+                    })
+                    .catch( err => {
+                        console.log(err)
+                    })
             if (r.name.match(regex))
                 return(<li key={r.id}>
-                    <Link onClick={toggle} to={{
-                        pathname: `/rooms/${r.id}`, 
-                        state: { id, name, description }
-                    }}>
-                        {r.name}
-                    </Link>
+                    <p>{r.title}</p>
+                    <p>{r.artst}</p>                 
+                    <Form action={add} method='post'>
+                    <Button type='submit' onClick={toggle} formmethod='post'>
+                        Add To Playlist
+                    </Button>
+                    </Form>
                 </li>)
         })
         return <ul>{options}</ul>
