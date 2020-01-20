64664a9ffdc7f45f5d49d309fea9d417aae029a1
Gabriella Algecera <gabriellaalgecera@Brians-MBP.domain>
2020-01-19 21:52:45 -0700

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,64 @@
 1.times do
     room = Room.create(
+        user_id: 1,
         name: 'Rock Room',
-        description: 'This room rocks')
+        description: 'This room rocks'),
 
-    3.times do
+    1.times do
         Playlist.create(
             room_id: room.id,
-            name: 'stuff',
+            name: '90s Rock',
             description: 'playlist stuff',
-            explicit: true
+            explicit: true,
         )
+    1.times do
+        Playlist.create(
+            room_id: room.id,
+            name: '80s Rock',
+            description: 'playlist stuff',
+            explicit: true,
+        )
+    1.times do
+        Playlist.create(
+            room_id: room.id,
+            name: '50s Rock',
+            description: 'playlist stuff',
+            explicit: false
+        )
+    end
+    end
+    end
+end
+
+1.times do
+    room = Room.create(
+        user_id: 1,
+        name: 'Pop Room',
+        description: 'This room is popular'),
+
+    1.times do
+        Playlist.create(
+            room_id: room.id,
+            name: '90s Pop',
+            description: 'playlist stuff',
+            explicit: true,
+        )
+    1.times do
+        Playlist.create(
+            room_id: room.id,
+            name: '80s Pop',
+            description: 'playlist stuff',
+            explicit: true,
+        )
+    1.times do
+        Playlist.create(
+            room_id: room.id,
+            name: '2000s Pop',
+            description: 'playlist stuff',
+            explicit: false
+        )
+    end
+    end
     end
 end
 
